4bbb0f16f49ba4c9b5154e4446b5c95b24319987
Lautenschlager <tai.romani@hotmail.com>
2020-04-17 00:23:33 -0300

    Update NPC.lua

--- NPC.lua
+++ NPC.lua
@@ -1,19 +1,5 @@
+-- Requires loop.lua <https://github.com/a801-luadev/useful-stuff/blob/master/loop.lua>
 -- Requires Callback.lua <https://github.com/a801-luadev/classes/blob/master/Callback.lua>
-
-local loop = function(f, ticks, ...)
-	local timers, index = { }, 0
-	local addTimer = function(_, ...)
-		index = index + 1
-		timers[index] = system.newTimer(f, 1000, true, ...)
-	end
-
-	local seconds = 1000 / ticks
-	for timer = 0, 1000 - seconds, seconds do
-		system.newTimer(addTimer, 1000 + timer, false, ...)
-	end
-
-	return timers
-end
 
 local npc
 do
